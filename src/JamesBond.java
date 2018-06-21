01f950573e50daf812920770f69ac3b0b95723ca
League Student <league@iMac-6.attlocal.net>
2018-06-21 14:04:22 -0700

    JamesBond

--- src/JamesBond.java
+++ src/JamesBond.java
@@ -2,8 +2,14 @@
 public class JamesBond {
 
 	public static void main(String[] args) {
-		Vault v = new Vault(1939020201);
-
+		Vault v = new Vault(200);
+		for (int i = 1; i<1000000; i++) {
+		boolean b = v.tryCode(i,v);
+		if (b == true) {
+			System.out.println("Cracked It!");
+		}
+		else if (b == false){
+			System.out.println("Locked.");
+		}
 	}
-
-}
+}}
--- src/Vault.java
+++ src/Vault.java
@@ -4,4 +4,13 @@ public class Vault {
 
 	Vault(int code){
 		this.code = code;
+}
+
+boolean tryCode(int guess, Vault va){
+	if(guess == va.code) {
+		return true;
+	}
+	else {
+		return false;
+}
 }}
